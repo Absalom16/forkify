8c0ab7347bcc13da228eef3d667cc894ccacf1b8
Absalom16 <absalomlihasi@gmail.com>
2023-05-24 11:02:09 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '283c7c42-89c8-4a46-b9f0-68c4a218aa2e'
+export const MODAL_CLOSE_SEC = 2.5;
